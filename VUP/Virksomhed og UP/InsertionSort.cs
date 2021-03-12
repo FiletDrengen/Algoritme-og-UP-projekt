5b0a36199e5fa7f498b47c44b6d82372fd68e15c
kre1e <olesendk@hotmail.com>
2021-03-12 10:59:49 +0100

    123

--- VUP/Virksomhed og UP/InsertionSort.cs
+++ VUP/Virksomhed og UP/InsertionSort.cs
@@ -6,5 +6,30 @@ namespace Virksomhed_og_UP
 {
     internal class InsertionSort
     {
+ 
+            static void Main(string[] args)
+            {
+                int[] numbers = new[] { 9, 3, 2, 6, 1, 4, 8, 5, 7 };
+
+                Insertionsort(numbers);
+            }
+
+            private static void Insertionsort(int[] numbers)
+            {
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    int val = numbers[i];
+                    int pointer = i;
+
+                    while (pointer > 0 && val < numbers[pointer - 1])
+                    {
+                        numbers[pointer] = numbers[pointer - 1];
+                        pointer = pointer - 1;
+                    }
+
+                    numbers[pointer] = val;
+                }
+            }
+
     }
 }
