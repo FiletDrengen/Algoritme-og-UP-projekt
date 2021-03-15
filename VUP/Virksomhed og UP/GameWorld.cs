f76bccd6f59d73daa8efaf4fa24d23f733d7e156
Hoffe <christian.hoffmann.thomsen@gmail.com>
2021-03-15 12:57:45 +0100

    Mah

--- VUP/Virksomhed og UP/GameWorld.cs
+++ VUP/Virksomhed og UP/GameWorld.cs
@@ -12,7 +12,7 @@ namespace Virksomhed_og_UP
         public List<int> numbers = new List<int>() { 6, 4, 9, 5, 2, 225, 22, 45, 5, 34, 8, 7, 10, 40, 30, 25, 34, 54, 8, 5, 43, 22, };
         private List<Brikker> brikker = new List<Brikker>();
         public static Dictionary<string, Texture2D> sprites = new Dictionary<string, Texture2D>();
-        private bool isSort = false;
+        private Texture2D Baggrund;
 
         public GameWorld()
         {
@@ -37,6 +37,7 @@ namespace Virksomhed_og_UP
             {
                 brikker.Add(new Brikker(numbers[i], i));
             }
+            Baggrund = Content.Load<Texture2D>("Baggrund");
 
             // TODO: use this.Content to load your game content here
         }
@@ -46,35 +47,22 @@ namespace Virksomhed_og_UP
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (Keyboard.GetState().IsKeyDown(Keys.NumPad1))
-            {
+            if (Keyboard.GetState().IsKeyDown(Keys.F1))
                 numbers = QuickSort.Quick(numbers);
-                brikker.Clear();
-                for (int i = 1; i < numbers.Count; i++)
-                {
-                    brikker.Add(new Brikker(numbers[i], i));
-                }
-            }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.NumPad2))
-            {
+            if (Keyboard.GetState().IsKeyDown(Keys.F2))
                 numbers = InsertionSort.Insertionsort(numbers);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.F3))
+                numbers = Bubblesort.bubblesort(numbers);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.F1) || Keyboard.GetState().IsKeyDown(Keys.F2) || Keyboard.GetState().IsKeyDown(Keys.F3))
+            {
                 brikker.Clear();
                 for (int i = 1; i < numbers.Count; i++)
-                {
                     brikker.Add(new Brikker(numbers[i], i));
-                }
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.NumPad3))
-            {
-                numbers = Bubblesort.bubblesort(numbers);
-                brikker.Clear();
-                for (int i = 1; i < numbers.Count; i++)
-                {
-                    brikker.Add(new Brikker(numbers[i], i));
-                }
-            }
             base.Update(gameTime);
         }
 
@@ -87,6 +75,9 @@ namespace Virksomhed_og_UP
                 item.Draw(_spriteBatch);
                 _spriteBatch.End();
             }
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Baggrund, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
